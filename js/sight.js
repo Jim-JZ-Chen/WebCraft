f5c5a6b008c30a27049071dbb4ea93cd5b832094
LAPTOP-NN6DMM7E\Jim <dfjhde@gmail.com>
2018-07-08 23:27:01 +1000

    Revert "add full screen feature"
    This reverts commit 363fbf13eb1bbed38c9ac0022e81efb3969b4f5f.

--- js/sight.js
+++ js/sight.js
@@ -13,8 +13,6 @@ class Sight
 					<span style="font-size:40px">Click to play</span>
 					<br />
 					(W, A, S, D = Move, left click to destroy block, right click to place block)
-					<br />
-					<button id="FullScreen">Full Screen </button>
 				</div>
 
 			</div>
@@ -34,8 +32,6 @@ class Sight
 		//this two div element is the crosshair on the center of screen
 		var sight1 = document.getElementById( 'sight1' );//horiztal
 		var sight2 = document.getElementById( 'sight2' );//vertial
-
-		var FullScreenBtn = document.getElementById( 'FullScreen' );//Full Screen Button
 		
 		//hide the crosshair
 		blocker.style.display = 'block';
@@ -94,11 +90,6 @@ class Sight
 			element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
 			element.requestPointerLock();
 
-		}, false );
-
-		FullScreenBtn.addEventListener( 'click', function ( event ) 
-		{
-			FullScreen.requestFullScreen();
 		}, false );
 	}
 }
